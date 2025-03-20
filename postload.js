d7320d7cb332585608553227deaeaf6b921384d6
ChristopherStraface <167484834+ChristopherStraface@users.noreply.github.com>
2025-03-20 16:48:12 -0400

    Tweaked delay timing
    Tweaked delay timing for 'Donkey Kong' to match the timing of the other characters' functions

--- postload.js
+++ postload.js
@@ -101,7 +101,7 @@ function displayZodInfo(whichOne) {
             zImageObj.src = 'Char_Images/Donkey Kong.png';
             zDateRangeObj.innerHTML = 'January 20th - February 18th';
             zDescription.innerHTML = 'DK, the king of swing! A fun-loving ape who loves to throw down with a party, he can quickly switch to a fit of rage and throw hands with anyone who threatens those he loves. He’s the last thing- primate or person- that you ever want to upset, because despite his loose exterior, there’s no one who will fight harder to protect his family and those he holds dear.';
-            removeHideMe(displayAreaObj, 500);
+            removeHideMe(displayAreaObj, 250);
             for (let i = 0; i < charButtonsArray.length; i++) {
                 charButtonsArray[i].classList.replace("charButton", "disableMe");
             }
